393b841881cf3a8dec3b6db1dfcf4a183335b93a
Matthew Vaden <matthewvaden@gmail.com>
2018-05-13 01:20:34 -0400

    The image is smaller, now fits within the view.

--- src/views/Image.js
+++ src/views/Image.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Me from '../assets/me.jpg';
+import '../styles/Image.css';
 
 const Myself = () => {
-  return <img alt='Myself' src={Me} />
+  return <img className='sanFrancisco' alt='Myself' src={Me} />
 }
 
 export default Myself;
